3fbb37ab918c225fb5617b398688ba620f46be46
Orifish <2437238986@qq.com>
2023-06-18 21:07:54 +0800

    Update grid sync·

--- CasualSelfAttention_Foward.cu
+++ CasualSelfAttention_Foward.cu
@@ -5,7 +5,7 @@
 #define THREADS_PER_BLOCK 1024
 #define DIVUP(m, n) ((m) / (n) + ((m) % (n) > 0))
 
-
+__device__ long wait_num;
 /*
 __global__ void SortCopy_Kernal(float* Sort_Matrix,int N,int C,float* QKV,int* index_num,int* class_index,int* sort_num,int* Orign){
     int blk_idx = blockIdx.x;
@@ -67,7 +67,9 @@ __global__ void CasualSA_Kernal(float* QKV,int N,int C,float* output,int* class_
             }
             // 得到了矩阵成积结果了，开始Softmax过程，每个Thread负责一个输出元素。到这里的已经是在需要输出的范围内的数据了。
             atomicAdd(&Block_sum,exp(mid_NN));
-            __syncthreads();        // 此处需要保证一个block一定是一行
+            waitnum += 1;       // 开始等待
+            while(waitnum<N*N);     // 同步整个块
+            // __syncthreads();        // 此处需要保证一个block一定是一行，这块应该不行。
             // Softmax完成，注意，这里感觉不行，因为默认了一个Block对应一行，具体分配规则待考虑。
             output[row*C+col] = 0;
             int row_target = Orign[row];
@@ -84,8 +86,8 @@ void casualSA_kernel_forward_launcher(int class_num,int N,int C, int* class_inde
 
     // dim3 blocks(DIVUP(N * C, THREADS_PER_BLOCK)); // blockIdx.x(col), blockIdx.y(row)
     // dim3 threads(THREADS_PER_BLOCK);
-    dim3 blocks(N); // blockIdx.x(col), blockIdx.y(row)
-    dim3 threads(N);
+    dim3 blocks(DIVUP(m, n)); // blockIdx.x(col), blockIdx.y(row)
+    dim3 threads(THREADS_PER_BLOCK);
 
     /*
     float* Sort_Matrix;
@@ -97,7 +99,8 @@ void casualSA_kernel_forward_launcher(int class_num,int N,int C, int* class_inde
     SortCopy_Kernal<<<blocks,threads>>>(Sort_Matrix,N,C,QKV,index_num,class_index,sort_num,Orign);        // 注意index_num必须要已经降序排序完了。
     cudaDeviceSynchronize();
     */
-
+    long value=0;
+    cudaMemcpyToSymbol(wait_num, &value, sizeof(long));     // 初始化wai_num
     CasualSA_Kernal<<<blocks,threads>>>(QKV,N,C,output,class_index,index_num,class_num,Origin);     
     // cudaFree(Sort_Matrix); 
     // cudaFree(Origin);
